a57aea4981c8e4e0b4c5e1b6bb81346ef0add8d3
TextError <savinraul1983@yahoo.com>
2019-01-16 13:25:34 +0000

    register user successful

--- client/src/redux/actions/register_user.js
+++ client/src/redux/actions/register_user.js
@@ -5,7 +5,7 @@ import { GET_ERRORS } from './types';
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .get('/api/users/register/', userData)
+    .post('/api/users/register/', userData)
     .then(res => history.push('/login'))
     .catch(err => dispatch({
       type: GET_ERRORS,
