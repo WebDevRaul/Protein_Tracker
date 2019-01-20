b0a9ac2c26106402a38b91a1a87bd68cc9e77733
TextError <savinraul1983@yahoo.com>
2019-01-20 17:58:59 +0000

    guest and user navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classnames  from 'classnames';
 import { Link } from 'react-router-dom';
+
+// Redux
+import { connect } from 'react-redux'
 
 class Navbar extends Component {
   constructor() {
@@ -14,6 +18,48 @@ class Navbar extends Component {
 
   render() {
     const { show } = this.state;
+    const { isAuthenticated, user } = this.props.auth;
+
+    // authenticated users
+    const authLinks = (
+      <ul className="navbar-nav ml-auto mr-5">
+        <li className="nav-item active mr-3 mt-3">
+          <Link 
+            to='/dashboard' 
+            className='nav-link'
+            onClick={this.onClick}
+          ><span>Dashboard</span></Link>
+        </li>
+        <li className="nav-item active mr-3 mt-3">
+          <Link 
+            to='/logout' 
+            className='nav-link'
+            onClick={this.onClick}
+          ><span>Logout</span></Link>
+        </li>
+      </ul>
+    );
+
+    // guest users
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto mr-5">
+        <li className="nav-item active mr-3 mt-3">
+          <Link 
+            to='/register' 
+            className='nav-link'
+            onClick={this.onClick}
+          ><span>Register</span></Link>
+        </li>
+        <li className="nav-item active mr-3 mt-3">
+          <Link 
+            to='/login' 
+            className='nav-link'
+            onClick={this.onClick}
+          ><span>Login</span></Link>
+        </li>
+      </ul>
+    );
+    
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
         <button
@@ -24,26 +70,21 @@ class Navbar extends Component {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className={classnames('collapse navbar-collapse', { 'show' : show })}>
-          <ul className="navbar-nav ml-auto mr-5">
-            <li className="nav-item active mr-3 mt-3">
-              <Link 
-                to='/register' 
-                className='nav-link'
-                onClick={this.onClick}
-              ><span>Register</span></Link>
-            </li>
-            <li className="nav-item active mr-3 mt-3">
-              <Link 
-                to='/login' 
-                className='nav-link'
-                onClick={this.onClick}
-              ><span>Login</span></Link>
-            </li>
-          </ul>
+          {isAuthenticated ? authLinks : guestLinks}
         </div>
       </nav>
     )
   }
-}
+};
 
-export default Navbar;
+Navbar.propTypes = {
+  errors: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {})(Navbar);
