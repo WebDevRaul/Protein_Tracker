ea978379379e82848580aa7cf2e7ca1f1dcc65b4
TextError <savinraul1983@yahoo.com>
2019-01-23 17:12:08 +0000

    back end item save to db

--- models/Item.js
+++ models/Item.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 
 // Create Item schema
 const ItemSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
   product_name: {
     type: String,
     required: true
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router  = express.Router();
+const passport = require('passport')
+
+// Item Model
+const Item = require('../../models/Item');
 
 
 // Validate
@@ -7,6 +11,23 @@ const router  = express.Router();
 // @route   POST api/items
 // @desc    Create item
 // @access  Private
+router
+  .post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    // Validation
+
+    // Create the item
+    const item = new Item({
+      user: req.user.id,
+      product_name: req.body.product_name,
+      protein: req.body.protein,
+      carbohydrates: req.body.carbohydrates,
+      fat: req.body.fat,
+      calories: req.body.calories
+    });
+
+    // Save
+    item.save().then(item => res.json(item));
+  });
 
 
 
