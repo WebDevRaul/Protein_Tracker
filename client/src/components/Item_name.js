b1cf2099431e856166c66628d0e71d4bc8a77cd8
TextError <savinraul1983@yahoo.com>
2019-01-25 19:15:14 +0000

    basic item

--- client/src/components/Item_name.js
+++ client/src/components/Item_name.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// Redux
+import { connect } from 'react-redux';
+
+// Common
+import isEmpty from './common/isEmpty';
 
 class ItemName extends Component {
   render() {
+    let item;
+    const { items } = this.props.items;
+    // Check for empty items
+    if (!isEmpty(items)) {
+      item = items[0].map(i => 
+        <div key={i.product_name}>
+          <p>{i.product_name}</p>
+          <p>{i.fat}</p>
+        </div>);
+    }
     return (
       <div>
-        .map db item
+        {item}
       </div>
     )
   }
 };
 
-export default ItemName;
+ItemName.propTypes = {
+  items: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  items: state.items
+});
+
+export default connect( mapStateToProps, {} )(ItemName);
