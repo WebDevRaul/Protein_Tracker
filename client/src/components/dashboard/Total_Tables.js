dbd66aa9c955c74902c954b67bb605f37698c149
TextError <savinraul1983@yahoo.com>
2019-02-09 13:54:17 +0000

    calc total calories,protein,fat and carbs from all tables

--- client/src/components/dashboard/Total_Tables.js
+++ client/src/components/dashboard/Total_Tables.js
@@ -5,6 +5,44 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Total_Tables extends Component {
+
+  // Sum up the prod vals
+  sumAll = (...args) => {
+    let sum = 0;
+    for (let arg of args) sum += arg;
+    return sum;
+  };
+
+  totalFunc = () => {
+    const { breakfast, lunch, diner, snack } = this.props.totalSum;
+    let calories = []; 
+    let protein = [];
+    let fat = [];
+    let carbohydrates = [];
+
+    // Collect all data
+    calories.push(breakfast.calories, lunch.calories, diner.calories, snack.calories)
+    protein.push(breakfast.protein, lunch.protein, diner.protein, snack.protein)
+    fat.push(breakfast.fat, lunch.fat, diner.fat, snack.fat)
+    carbohydrates.push(breakfast.carbohydrates, lunch.carbohydrates, diner.carbohydrates, snack.carbohydrates)
+
+    // Calc the sum
+    const totalCalories = this.sumAll(...calories);
+    const totalProtein = this.sumAll(...protein);
+    const totalFat = this.sumAll(...fat);
+    const totalCarbohydrates = this.sumAll(...carbohydrates);
+
+    // Return Obj sum
+    const totalVals = [
+      {totalCalories},
+      {totalProtein},
+      {totalFat},
+      {totalCarbohydrates}
+    ];
+
+    return totalVals;
+  }
+
   render() {
     return (
       <div>
