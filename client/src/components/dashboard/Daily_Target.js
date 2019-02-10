26dea7b69bca8c5558bd7d1f896969e40ec43996
TextError <savinraul1983@yahoo.com>
2019-02-10 12:45:33 +0000

    basic form

--- client/src/components/dashboard/Daily_Target.js
+++ client/src/components/dashboard/Daily_Target.js
@@ -4,20 +4,35 @@ import classnames from 'classnames';
 
 // Redux
 import { connect } from 'react-redux';
+import CardFieldGroup from '../common/components/CardFieldGroup';
 
 class DailyTarget extends Component {
   constructor() {
     super();
     this.state = {
-      form: false,
+      form: true,
+      age: '',
+      sex: '',
+      height: '',
+      weight: '',
+      activity: '',
     }
   };
 
   calculate = () => {
     this.setState({ form: !this.state.form })
   }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = (e) => {
+    e.preventSubmint();
+  }
   render() {
-    const { form } = this.state;
+    const { form, errors } = this.state;
+
     return (
       <div>
         <div className="card paper">
@@ -27,7 +42,77 @@ class DailyTarget extends Component {
             >edit</span>
           </div>
           <div className={classnames("card-body", {'d-none' : !form})}>
-            <p>body</p>
+            <div className="spacial-card text-capitalize font-weight-bold">
+              <form noValidate onSubmit={this.onSubmit}>
+                <ul className="navbar list-inline">
+                  <li className="list-inline-item">
+                    <CardFieldGroup
+                      name='age'
+                      value={this.state.age}
+                      placeholder='Age'
+                      onChange={this.onChange}
+                      error={errors}
+                    />
+                  </li>
+                  <li className="list-inline-item">
+                    <div className="form-group">
+                      <select 
+                        className="form-control custom-select form-control-xsm"
+                        name='sex'
+                        value={this.state.sex}
+                        onChange={this.onChange}
+                      >
+                        <option>Sex</option>
+                        <option value='male'>Male</option>
+                        <option value='female'>Female</option>
+                      </select>
+                    </div>
+                  </li>
+                  <li className="list-inline-item">
+                    <CardFieldGroup
+                      name='height'
+                      value={this.state.height}
+                      placeholder='Height'
+                      onChange={this.onChange}
+                      error={errors}
+                    />
+                  </li>
+                  <li className="list-inline-item">
+                    <CardFieldGroup
+                      name='weight'
+                      value={this.state.weight}
+                      placeholder='Weight'
+                      onChange={this.onChange}
+                      error={errors}
+                    />
+                  </li>
+                  <li className="list-inline-item">
+                    <div className="form-group">
+                      <select 
+                        className="form-control custom-select form-control-xsm"
+                        name='activity'
+                        value={this.state.activity}
+                        onChange={this.onChange}
+                      >
+                        <option>Activity</option>
+                        <option value='easy'>easy</option>
+                        <option value='medium'>medium</option>
+                        <option value='hard'>hard</option>
+                      </select>
+                    </div>
+                  </li>
+                  <li className="list-inline-item">
+                    <CardFieldGroup
+                      name='product_name'
+                      type='button'
+                      value='Save'
+                      onChange={this.onChange}
+                      error={errors}
+                    />
+                  </li>
+                </ul>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -36,9 +121,11 @@ class DailyTarget extends Component {
 };
 
 DailyTarget.propTypes = {
-
+  errors: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  errors: state.errors
+});
 
 export default connect( mapStateToProps, {} )(DailyTarget);
