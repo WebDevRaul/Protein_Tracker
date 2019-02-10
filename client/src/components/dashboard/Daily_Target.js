59904c34bd451868aaee482f8f07adf2ece37911
TextError <savinraul1983@yahoo.com>
2019-02-10 13:53:46 +0000

    calculate calories

--- client/src/components/dashboard/Daily_Target.js
+++ client/src/components/dashboard/Daily_Target.js
@@ -6,6 +6,9 @@ import classnames from 'classnames';
 import { connect } from 'react-redux';
 import CardFieldGroup from '../common/components/CardFieldGroup';
 
+// Common
+import isEmpty from '../common/isEmpty';
+
 class DailyTarget extends Component {
   constructor() {
     super();
@@ -16,10 +19,14 @@ class DailyTarget extends Component {
       height: '',
       weight: '',
       activity: '',
+      calories: '',
+      protein: '',
+      fat: '',
+      carbohydrates: '',
     }
   };
 
-  calculate = () => {
+  showForm = () => {
     this.setState({ form: !this.state.form })
   }
 
@@ -29,6 +36,15 @@ class DailyTarget extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
+    const { weight, height, age, sex, activity } = this.state;
+    let calories;
+    if (!isEmpty(sex) && sex === 'male') {
+      calories=(66.4730+(13.7516 * Number(weight)) + (5.0033 * Number(height)) - (6.7550 * Number(age))) * Number(activity);
+    };
+    if (!isEmpty(sex) && sex === 'female') {
+      calories=(655.0955 + (9.5634 * Number(weight)) + (1.8496 * Number(height))-(4.6756 * Number(age))) * Number(activity);
+    }
+    console.log(Math.ceil(calories).toString());
   }
   render() {
     const { form, errors } = this.state;
@@ -38,12 +54,12 @@ class DailyTarget extends Component {
         <div className="card paper">
           <div className="card-header bg-transparent">
             <span
-              onClick={this.calculate}
+              onClick={this.showForm}
             >edit</span>
           </div>
           <div className={classnames("card-body", {'d-none' : !form})}>
             <div className="spacial-card text-capitalize font-weight-bold">
-              <form noValidate>
+              <form noValidate onSubmit={this.onSubmit}>
                 <ul className="navbar list-inline">
                   <li className="list-inline-item">
                     <CardFieldGroup
@@ -78,9 +94,11 @@ class DailyTarget extends Component {
                         onChange={this.onChange}
                       >
                         <option>Activity</option>
-                        <option value='easy'>easy</option>
-                        <option value='medium'>medium</option>
-                        <option value='hard'>hard</option>
+                        <option value='1.2'>Little to no exercise</option>
+                        <option value='1.375'>1–3 days per week</option>
+                        <option value='1.55'>3–5 days per week</option>
+                        <option value='1.725'>6–7 days per week</option>
+                        <option value='1.9'>Twice per day</option>
                       </select>
                     </div>
                   </li>
@@ -107,10 +125,9 @@ class DailyTarget extends Component {
                   <li className="list-inline-item">
                     <div className='form-group'>
                       <input
-                        type='button'
+                        type='submit'
                         className='form-control form-control-xsm'
                         value='Save'
-                        onClick={this.onSubmit}
                       />
                     </div>
                   </li>
