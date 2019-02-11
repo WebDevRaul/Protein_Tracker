0e82db332a157f572e02492b11c93fc680625846
TextError <savinraul1983@yahoo.com>
2019-02-11 19:46:48 +0000

    to finish later

--- client/src/components/dashboard/Actual.js
+++ client/src/components/dashboard/Actual.js
@@ -3,15 +3,10 @@ import PropTypes from 'prop-types';
 
 // Redux
 import { connect } from 'react-redux';
-import { collectActual } from '../../redux/actions/dashboard';
-
-// Common
-import isEmpty from '../common/isEmpty';
 
 class Actual extends Component {
   render() {
     const { actual } = this.props.calculator
-    // const numbers = actual.map(i => <li key={Object.entries(i)} className='list-inline-item'>{Object.keys(i)} :  {Object.values(i)}</li>)
     const number = Object.entries(actual).map(i => <li key={Object.entries(i)} className='list-inline-item'>
     {Object.values(i)[0]}: {Object.values(i)[1]}
     </li>);
