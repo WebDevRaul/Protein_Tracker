3b617864aa9874ae255f860ff58dde7768640138
TextError <savinraul1983@yahoo.com>
2019-02-23 20:34:45 +0000

    basic footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,10 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// Components
+import Logo from './Logo';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        Footer
+      <div className='footer'>
+        <div className='container'>
+          <div className='row border rounded border-primary'>
+            <div className='col p-3'>
+              <div className='p-2 border-right border-primary'>
+                <Logo />
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='col'>
+                  <p>Info</p>
+                </div>
+              </div>
+              <div className='row'>
+                <div className='col'>
+                  <p>Links</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
