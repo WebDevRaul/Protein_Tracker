9f8110f28b7c00cd71195e01d17b3bddf3deb0e3
TextError <savinraul1983@yahoo.com>
2019-02-23 20:55:40 +0000

    basic map for footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // Components
 import Logo from './Logo';
@@ -10,7 +11,7 @@ export default class Footer extends Component {
         <div className='container'>
           <div className='row border rounded border-primary'>
             <div className='col p-3'>
-              <div className='p-2 border-right border-primary'>
+              <div className='p-2'>
                 <Logo />
               </div>
             </div>
@@ -22,8 +23,36 @@ export default class Footer extends Component {
               </div>
               <div className='row'>
                 <div className='col'>
-                  <p>Links</p>
+                  <div className='text-center m-auto'>
+                    <span className='ml-2 mr-2'>
+                      <Link to={'/'} target='_blank'>
+                        <i className='m-2 fab fa-facebook-f fa-1x'></i>
+                      </Link>
+                    </span>
+                    <span className='ml-2 mr-2'>
+                      <Link to={'/'} target='_blank'>
+                        <i className='m-2 fab fa-twitter fa-1x'></i>
+                      </Link>
+                    </span>
+                    <span className='ml-2 mr-2'>
+                      <Link to={'/'} target='_blank'>
+                        <i className='m-2 fab fa-instagram fa-1x'></i>
+                      </Link>
+                    </span>
+                    <span className='ml-2 mr-2'>
+                      <Link to={'/'} target='_blank'>
+                        <i className='m-2 fab fa-linkedin-in fa-1x'></i>
+                      </Link>
+                    </span>
+                  </div>
                 </div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <div className='text-center font-weight-bold broder-top broder-success pt-4'>
+                <p>Copyright &copy; {new Date().getFullYear()} Protein-Tracker</p>
               </div>
             </div>
           </div>
