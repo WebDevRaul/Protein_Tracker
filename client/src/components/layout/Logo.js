5108fcab1b9cbc0ae9187511f709e0ded520c6d2
TextError <savinraul1983@yahoo.com>
2019-02-24 10:38:54 +0000

    finish later

--- client/src/components/layout/Logo.js
+++ client/src/components/layout/Logo.js
@@ -6,8 +6,19 @@ export default class Logo extends Component {
     return (
       <div className='mt-3 mr-5'>
         <Link to='/home'>
-          Logo
+          <p className='m-0'>Protien</p>
+          __________
+          <p>Tracker</p>
         </Link>
+        <Link to='/home'>
+          <p className='m-0'>Protien</p>
+          <p><span>--------</span>Tracker</p>
+        </Link>
+        <Link to='/home'>
+          <p className='ml-5 mb-0'>Protien</p>
+          <p><span>--------</span><span className=''>Tracker</span><span>--------</span></p>
+        </Link>
+
       </div>
     )
   }
