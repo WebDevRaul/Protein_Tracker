33f6f930134022dd9e0b0a0a50734a6911ce4bd6
TextError <savinraul1983@yahoo.com>
2019-03-19 17:27:12 +0000

    10 default items

--- client/src/components/common/defaultItems.js
+++ client/src/components/common/defaultItems.js
@@ -1,34 +1,104 @@
 const Items = [
   {
     id: 1,
-    product_name: 'test1',
-    quantity: '1',
-    type: 'pc.',
-    calories: '1',
-    protein: '1',
-    fat: '1',
-    carbohydrates: '1'
+    product_name: 'Cranberries',
+    quantity: '100',
+    type: 'gr.',
+    protein: '1.1',
+    carbohydrates: '21.0',
+    fat: '0.5',
+    calories: '85.0'
   },
   {
     id: 2,
-    product_name: 'test2',
+    product_name: 'Pepper',
     quantity: '1',
     type: 'pc.',
-    calories: '1',
-    protein: '1',
-    fat: '1',
-    carbohydrates: '2'
+    protein: '1.0',
+    carbohydrates: '6.0',
+    fat: '0.2',
+    calories: '24.0'
   },
   {
     id: 3,
-    product_name: 'test3',
+    product_name: 'Hazelnuts',
+    quantity: '100',
+    type: 'gr.',
+    protein: '15.4',
+    carbohydrates: '17.8',
+    fat: '51.3',
+    calories: '684.5'
+  },
+  {
+    id: 4,
+    product_name: 'Avocado',
     quantity: '1',
     type: 'pc.',
-    calories: '1',
-    protein: '1',
-    fat: '1',
-    carbohydrates: '3'
-  }
+    protein: '4.0',
+    carbohydrates: '17.0',
+    fat: '29.0',
+    calories: '322.0'
+  },
+  {
+    id: 5,
+    product_name: 'Banana',
+    quantity: '1',
+    type: 'pc.',
+    protein: '1.3',
+    carbohydrates: '27.0',
+    fat: '0.4',
+    calories: '105.0'
+  },
+  {
+    id: 6,
+    product_name: 'Broccoli boiled',
+    quantity: '100',
+    type: 'gr.',
+    protein: '2.4',
+    carbohydrates: '7.0',
+    fat: '0.4',
+    calories: '35.0'
+  },
+  {
+    id: 7,
+    product_name: 'Cottage cheese',
+    quantity: '100',
+    type: 'gr.',
+    protein: '11.0',
+    carbohydrates: '3.4',
+    fat: '4.3',
+    calories: '98.0'
+  },
+  {
+    id: 8,
+    product_name: 'Brine telemea',
+    quantity: '100',
+    type: 'gr.',
+    protein: '25.0',
+    carbohydrates: '0.7',
+    fat: '10.5',
+    calories: '197.0'
+  },
+  {
+    id: 9,
+    product_name: 'Turkey meat',
+    quantity: '100',
+    type: 'gr.',
+    protein: '17.0',
+    carbohydrates: '4.0',
+    fat: '2.0',
+    calories: '104.0'
+  },
+  {
+    id: 10,
+    product_name: 'Chicken meat',
+    quantity: '100',
+    type: 'gr.',
+    protein: '25.0',
+    carbohydrates: '0.0',
+    fat: '13.0',
+    calories: '219.0'
+  },
 ];
 
 export default Items;
