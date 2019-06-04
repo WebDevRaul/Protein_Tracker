4da4898888140def2195a5ff3ff4f2e7cc5d0adc
TextError <savinraul1983@yahoo.com>
2019-06-04 18:02:25 +0100

    home page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -84,7 +84,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-lg navbar-light">
+      <nav className="navbar navbar-expand-lg">
         <Logo />
         <button
           className="navbar-toggler" 
--- client/src/components/links/Home.js
+++ client/src/components/links/Home.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        home
+      <div className='home'>
+        <div className='container d-flex'>
+          <div className='row no-gutters m-auto'>
+            <div className='col d-flex'>
+              <div className='m-auto'>
+                <div className='home-text m-auto text-center'>
+                  <h1 className='mb-5'>Macro Calculator</h1>
+                  <h5>New member? <i><Link to='/register'> - Register - </Link></i></h5>
+                  <h5>or</h5>
+                  <h5><i><Link to='/login'> - Sign in - </Link></i></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
