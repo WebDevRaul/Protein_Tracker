b4236d416b20244f824a8b7d6d3a75abe81f6f1b
TextError <savinraul1983@yahoo.com>
2019-06-05 16:49:28 +0100

    navbar btn

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -84,7 +84,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg navbar-light">
         <Logo />
         <button
           className="navbar-toggler" 
