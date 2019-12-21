ff519adc6f746966e5fafcb4cf073ac8f9363bb9
TextError <savinraul1983@yahoo.com>
2019-12-21 14:35:03 +0200

    Added favicon

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -7,12 +7,14 @@ const Home = ({ history }) => {
   return (
     <div className='home m-2 d-flex flex-column justify-content-center align-items-center'>
       <div className='ml-3 text-center'>
-        <h3 className='mb-4 mt-4'>Macro Calculator</h3>
+        <h3 className='mt-3 mb-0'>Protein Tracker</h3>
+        <h3 className='mb-0'>&</h3>
+        <h3 className='mb-2'>Macro Calculator</h3>
         <h5>New member?</h5>
-        <h5 className='mt-3'>
-          <i onClick={() => history.push('/register')}>- Register -</i>
+        <h5 className='mt-2'>
+          <i onClick={() => history.push('/register')}>Register - </i>
           or
-          <i onClick={() => history.push('/sign-in')}> - Sign in - </i>
+          <i onClick={() => history.push('/sign-in')}> - Sign in</i>
         </h5>
       </div>
     </div>
