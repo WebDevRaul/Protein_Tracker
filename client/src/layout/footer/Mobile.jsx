706e1e7ee558f81e75f166096d5a972cc92fa143
TextError <savinraul1983@yahoo.com>
2019-12-22 12:12:24 +0200

    Mobile update

--- client/src/layout/footer/Mobile.jsx
+++ client/src/layout/footer/Mobile.jsx
@@ -5,7 +5,7 @@ const Mobile = () => {
     <div className='d-flex mb-2 ml-5'>
       <i className="fas fa-2x fa-mobile-alt text-secondary"></i>
       <div className='d-flex m-auto w-50'>
-        <p className='m-auto font-weight-bold'>555-555-5555</p>
+        <p className='m-auto font-weight-bold'>+373 781 91 635</p>
       </div>
   </div>
   )
